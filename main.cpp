bba4a5027fea08fcd67a4b3c28ba952d57afcbcf
MrZorg111 <yodji333@mail.ru>
2022-01-22 14:29:50 +0300

    First solution

--- main.cpp
+++ main.cpp
@@ -2,16 +2,32 @@
 #include <vector>
 
 std::vector<int> add(std::vector<int> a, int b) {
+    std::vector<int> tempo(a.size());
+    for(int i = 0, j = 1; i < a.size(); i++, j++) {
+        tempo[i] = a[j];
+        tempo[a.size()] = b;
+    }
+    return tempo;
 
 }
 
 int main() {
-    int n;
+    int n, element = 0;
     std::vector<int> vec(5);
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; ; i++) {
         std::cout << "Enter the array elements: \n";
         std::cin >> n;
-        vec[i] = n;
+        if(n == - 1) {
+            break;
+        }
+        if (element >= 5) {
+            vec = add(vec, n);
+        }
+        else {
+            vec[i] = n;
+            element++;
+            std::cout << element;
+        }
     }
 
     for (int t = 0; t < vec.size(); t++){
